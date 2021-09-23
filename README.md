fb23dd8a0e880fd21a06d14906410c017d279423
Will <anon-github@hinson.ai>
2021-09-23 19:34:07 -0400

    Added support for strongly-typed functions.

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
-# py-strong-typing
+# Python Strong Typing
+This is a runtime library that allows Python classes to enforce strong typing of their members at declaration and assignment and Python functions to have type-checked parameters and return values. It supplies a `@strongly_typed` decorator that may be applied to any class or function to enable type checking.
+
+## Installation
+This library is available via PyPI. It may be installed with the following pip command:
+
+```
+pip install py-strong-typing
+```
+
+## Usage
+
+### With Classes
+Any class may declare itself as having strongly-typed members using the `@strongly_typed` decorator:
+
+```Python
+from strong_typing import *
+
+@strongly_typed
+class example_class:
+    a: int
+    b: int
+
+print(example_class.a)    # None
+```
+
+Upon assignment, any member of an instance of the class that provides a type hint will be type checked on assignment:
+
+```Python
+example_inst = example_class()
+
+example_inst.a = 123
+print(example_inst.a)       # 123
+example_inst.a = 'hello'    # results in TypeError
+```
+
+Any members that do not have a type hint will not be checked. Also note that static assignments at the time of class declaration cannot be checked.
+
+```Python
+from strong_typing import *
+
+@strongly_typed
+class example_class:
+    a = 'hello'
+    b: list = 123   # valid. static assignment at declaration is not checked
+
+inst = example_class()
+inst.a = 123        # valid. 'a' has no hint so it is not checked
+```
+
+### With Functions
+Any Python function may declare its parameters and return type as strongly-typed using the `@strongly_typed` directive:
+
+```Python
+from strong_typing import *
+
+@strongly_typed
+def test(a: int, b: str) -> str:
+    return str(a) + b
+
+print(test(123, 'hello'))           # returns '123hello'
+print(test(10.0, 'invalid'))        # results in TypeError
+```
+
+As with strongly-typed classes, any parameters that lack a hint will not be checked.
+
+```Python
+from strong_typing import *
+
+@strongly_typed
+def test(a, b: str) -> str:
+    return str(a) + b
+
+print(test(10.0, 'not invalid'))    # returns '10.0not invalid' as 'a' is no longer checked.
+```
+
+This rule also applies to the return type hint:
+
+```Python
+from strong_typing import *
+
+@strongly_typed
+def test(a: int):
+    return a + 123
+
+@strongly_typed
+def test2(a: int) -> str:
+    return a + 123
+
+print(test(111))        # outputs '234'
+print(test2(111))       # results in TypeError
+```
--- strong_typing/__init__.py
+++ strong_typing/__init__.py
@@ -1,4 +1,58 @@
-from typing import get_type_hints
+from copy import copy
+from inspect import signature
+from typing import get_type_hints, Union
+from types import FunctionType as function
+
+class _typed_function:
+    __origcall__    = None
+    __params__      = None
+    __types__       = None
+
+    def __init__(self, func: function):
+        self.__origcall__ = func.__call__
+        self.__params__ = list(signature(func).parameters.keys())
+        self.__types__ = get_type_hints(func)
+        
+        # check for a return type
+        if 'return' in self.__types__.keys():
+            self.__returntype__ = self.__types__['return']
+
+    def __call__(self, *args, **kwargs):
+        params_copy = copy(self.__params__)
+
+        # type check all kwargs
+        for key in kwargs:
+            if key in self.__types__.keys():
+                if not issubclass(kwargs[key].__class__, self.__types__[key]):
+                    raise TypeError(
+                        'object of type \'' + str(kwargs[key].__class__.__name__)+ '\' cannot be assigned '
+                        'to parameter \'' + key + '\' of type \'' + str(self.__types__[key].__name__) + '\''
+                    )
+                params_copy.remove(key)
+            else:
+                params_copy.remove(key)
+
+        # type check all positional args
+        for n in range(len(args)):
+            if params_copy[n] in self.__types__.keys():
+                if not issubclass(args[n].__class__, self.__types__[params_copy[n]]):
+                    raise TypeError(
+                            'object of type \'' + str(args[n].__class__.__name__)+ '\' cannot be assigned '
+                            'to parameter \'' + params_copy[n] + '\' of type \'' + str(self.__types__[params_copy[n]].__name__) + '\''
+                    )
+
+        # call the function that we are wrapping
+        retval = self.__origcall__(*args, **kwargs)
+
+        # if a return type was specified, check the return type
+        if hasattr(self, '__returntype__'):
+            if not issubclass(retval.__class__, self.__returntype__):
+                raise TypeError(
+                            'object of type \'' + str(retval.__class__.__name__)+ '\' cannot be assigned '
+                            'to return value of type \'' + str(self.__returntype__.__name__) + '\''
+                    )
+
+        return retval
 
 def _checked_setattr(inst: object, name: str, attr: object) -> None:
     # check if this attribute has a type hint
@@ -21,26 +75,32 @@ def _checked_setattr(inst: object, name: str, attr: object) -> None:
             # if not, throw an error
             raise NameError('object has no attribute \'' + name + '\'')
 
-def strongly_typed(class_def: type) -> type:
+def strongly_typed(class_def: Union[type, function]):
     """
     A decorator that allows Python classes to enforce strong typing of their members
     and disallows assignment to attributes that have not been declared
     """
 
-    # define the __types__ member which will contain a direct reference to type hints
-    class_def.__types__ = get_type_hints(class_def)
+    # check if the received object is a class or a function
+    if issubclass(class_def.__class__, type):
+        # define the __types__ member which will contain a direct reference to type hints
+        class_def.__types__ = get_type_hints(class_def)
 
-    # ensure that all attributes are initialized. if not, initialize them to None
-    for key in class_def.__types__:
-        if key not in class_def.__dict__.keys():
-             setattr(class_def, key, None)
+        # ensure that all attributes are initialized. if not, initialize them to None
+        for key in class_def.__types__:
+            if key not in class_def.__dict__.keys():
+                setattr(class_def, key, None)
 
-    # swap out the existing __setattr__ function for our checked one. preserve
-    # the original as __origsetattr__
-    class_def.__origsetattr__ = class_def.__setattr__
-    class_def.__setattr__ = _checked_setattr
+        # swap out the existing __setattr__ function for our checked one. preserve
+        # the original as __origsetattr__
+        class_def.__origsetattr__ = class_def.__setattr__
+        class_def.__setattr__ = _checked_setattr
+    elif issubclass(class_def.__class__, function):
+        class_def = _typed_function(class_def)
+    else:
+        raise TypeError(
+            'cannot apply strongly_typed decorator to objects of type ' + str(type(class_def).__name__)
+        )
 
-    # simply return the class defintion
+    # simply return the class or function defintion
     return class_def
-
-globals()['a'] = 123
